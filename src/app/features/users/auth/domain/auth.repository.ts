e65afb563049fdc26dfac1750e49a9ded28356cf
Gersomsim <gesomhernandez@me.com>
2026-04-06 01:45:02 -0600

    Respository de auth

---
+++ src/app/features/users/auth/domain/auth.repository.ts
@@ -0,0 +1,9 @@
+import { Auth } from './auth.model'
+
+export interface AuthRepository {
+	forgotPassword(email: string): Promise<void>
+	login(email: string, password: string): Promise<Auth>
+	logout(): Promise<void>
+	register(email: string, password: string): Promise<void>
+	resetPassword(token: string, password: string): Promise<void>
+}
