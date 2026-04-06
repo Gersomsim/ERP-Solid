62dc7ba905b3e01251f086e34f9b15da0fa4f293
Gersomsim <gesomhernandez@me.com>
2026-04-06 16:00:20 -0600

    Inyeccion de providers por pagina

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
-import { DI_TOKEN } from '@core/providers'
-
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), ...DI_TOKEN],
+	providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 }
--- src/app/core/providers/di.token.ts
+++
@@ -1,5 +0,0 @@
-import { Provider } from '@angular/core'
-
-import { AuthTokenProvider } from '@features/users/auth/infra'
-
-export const DI_TOKEN: Provider[] = [AuthTokenProvider]
